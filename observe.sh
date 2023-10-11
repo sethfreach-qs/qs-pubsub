3773b7a2f9d7e5cd52df445418b80b38eda50503
Seth Freach <seth.freach@quantspark.com>
2023-10-11 15:24:53 +0100

    change defaul docker containers for project

--- observe.sh
+++ observe.sh
@@ -4,11 +4,11 @@ path=$(pwd)
 python=$(which python3)
 
 # start log monitors for the docker container logs
-osascript -e 'tell app "Terminal" to do script "docker logs --follow qs-pubsub-order-processor-1"'
-osascript -e 'tell app "Terminal" to do script "docker logs --follow qs-pubsub-payment-processor-1"'
-osascript -e 'tell app "Terminal" to do script "docker logs --follow qs-pubsub-city-london-1"'
-osascript -e 'tell app "Terminal" to do script "docker logs --follow qs-pubsub-city-manchester-1"'
-osascript -e 'tell app "Terminal" to do script "docker logs --follow qs-pubsub-city-york-1"'
+osascript -e 'tell app "Terminal" to do script "docker logs --follow ps-order-processor-1"'
+osascript -e 'tell app "Terminal" to do script "docker logs --follow ps-payment-processor-1"'
+osascript -e 'tell app "Terminal" to do script "docker logs --follow ps-city-london-1"'
+osascript -e 'tell app "Terminal" to do script "docker logs --follow ps-city-manchester-1"'
+osascript -e 'tell app "Terminal" to do script "docker logs --follow ps-city-york-1"'
 
 # run the original consumer.py script to observe the general logging output on that topic
 $python "${path}/consumer.py"
